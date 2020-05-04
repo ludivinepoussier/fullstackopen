e98fbb726b1ac82fad8fdf6758cfb58236c0e3e4
Ludivine Poussier <ludivinepoussier@hotmail.com>
2020-05-04 18:44:28 +0100

    Exercise Anecdotes Solution

--- part-1/anecdotes/src/index.js
+++ part-1/anecdotes/src/index.js
@@ -3,75 +3,79 @@ import ReactDOM from 'react-dom'
 
 const Button = ({ onClick, text }) => (
   <button onClick={onClick}>
-    {text}
+  {text}
   </button>
 )
-
+  
 const Vote = ({text, value}) => (
-<p>{text} {value}</p>
+  <p>{text} {value}</p>
 )
-
+    
 const Votes = ({vote}) => {
   return (
     <div>
-        <Vote text="Numbers of votes for this anecdote: " value={vote} />
+    <Vote text="Numbers of votes for this anecdote: " value={vote} />
     </div>
   )
 }
+      
+const AnecdoteSingle = ({ text }) => <p>"{text}"</p>
 
+const AnecdoteMulti = ({ arrText }) => arrText.map(text => < AnecdoteSingle key={text.toString()} text={text} />)
+      
 const MostVotes = ({vote}) => {
   const mostVote = Math.max(...vote)
-  const mostIndex = vote.indexOf(mostVote)
-  // filter valor do MostVote com condition
-
-  if (mostVote === 0) {
-    return (
-      <p>Please vote for your favorite anecdote.</p>
-    )
-  }
-  return (
-    <p>"{anecdotes[mostIndex]}"</p>
-  )
+  if (mostVote === 0) return  <p>Please vote for your favorite anecdote.</p>
+  
+  const mostIndexArr = vote
+  .map((val, idx) => ({ val, idx }))
+  .filter( ({val}) => val === mostVote)
+  .map( ({idx}) => idx )
+  
+  const response = mostIndexArr.map(mostIndex => { const batata = mostIndex; return anecdotes[batata]})
+  
+  return < AnecdoteMulti arrText={response}/>
 }
 
-const App = (props) => {
+      
+const App = ({anecdotes}) => {
   const [selected, setSelected] = useState(0)
   const [vote, setVote] = useState(new Array(6).fill(0))
   
   const random = (arr) => {
-     let next
-   do {
-     next = Math.floor(Math.random() * Math.floor(arr.length))
-   } while (next === selected)   
-   return next
+    let next
+    do {
+      next = Math.floor(Math.random() * Math.floor(arr.length))
+    } while (next === selected)   
+    return next
   }
-
+  
   const handleClickNext = () => {
     setSelected(random(anecdotes))
   }
-
+  
   const handleClickVote = () => {
     const copy = [...vote]
     copy[selected] += 1
     setVote(copy)
   }
-
+  
   return (
     <div>
-      <h1>The world of software engineering anecdotes</h1>
-      <h2>Our selection of anecdotes:</h2>
-      "{props.anecdotes[selected]}"
-      <Votes vote={vote[selected]} />
-      <p>
-        <Button onClick={handleClickNext} text='next one' />
-        <Button onClick={handleClickVote} text='vote' />
-      </p>
-      <h2>Anecdote with most votes:</h2>
-      <MostVotes vote={vote} anecdotes={props.anecdotes} />
+    <h1>The world of software engineering anecdotes</h1>
+    <h2>Our selection of anecdotes:</h2>
+    "{anecdotes[selected]}"
+    <Votes vote={vote[selected]} />
+    <p>
+    <Button onClick={handleClickNext} text='next one' />
+    <Button onClick={handleClickVote} text='vote' />
+    </p>
+    <h2>Anecdote(s) with most votes:</h2>
+    <MostVotes vote={vote} anecdotes={anecdotes} />
     </div>
   )
 }
-
+        
 const anecdotes = [
   'If it hurts, do it more often.',
   'Adding manpower to a late software project makes it later!',
@@ -80,7 +84,7 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
-
+        
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
   document.getElementById('root')
