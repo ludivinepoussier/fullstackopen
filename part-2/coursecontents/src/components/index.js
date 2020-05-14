7cf3a60149f24fb21b430ee7fd1a1fa223742ff9
Ludivine Poussier <ludivinepoussier@hotmail.com>
2020-05-14 09:21:54 +0100

    part-2 courseontents 2.5 indentation

--- part-2/coursecontents/src/components/index.js
+++ part-2/coursecontents/src/components/index.js
@@ -1,3 +1,3 @@
 import Course from './Course'
 
-export default Course
+export  { Course }
--- part-2/coursecontents/src/index.js
+++ part-2/coursecontents/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Course from './components'
+import { Course } from './components'
 
 const App = () => {
 
