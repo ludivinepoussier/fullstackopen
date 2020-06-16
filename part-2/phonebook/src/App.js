a5a34bb56830f5399556a13a833d9abe9145a874
Ludivine Poussier <ludivinepoussier@hotmail.com>
2020-06-16 15:16:36 +0100

    phonebook 2.6 first attempt

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,17 +1,42 @@
 import React, { useState } from 'react'
+import Name from './components/Name'
 
 const App = () => {
     const [persons, setPersons] = useState([
         { name: 'Arto Hellas' }
     ])
     const [newName, setNewName] = useState('')
+    const [showAll, setShowAll] = useState(true)
+
+    const addName = (event) => {
+        event.preventDefault()
+
+        const nameObject = {
+            content: newName,
+            id: persons.length + 1,
+        }
+
+        setPersons(persons.concat(nameObject))
+        setNewName('')
+    }
+
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
+
+    const namesToShow = showAll
+        ? persons
+        : persons.filter(name => name.important)
 
     return (
         <div>
             <h2>Phonebook</h2>
-            <form>
+            <form onSubmit={addName}>
                 <div>
-                    name: <input />
+                    name: 
+                    <input 
+                        value={newName}
+                        onChange={handleNameChange}/>
                 </div>
                 <div>debug: {newName}</div>
                 <div>
@@ -19,7 +44,11 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-      ...
+            <ul>
+                {namesToShow.map(name =>
+                    <Name key={name.id} name={name} />
+                )}
+            </ul>
         </div>
     )
 }
