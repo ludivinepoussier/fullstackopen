e59104bc24db2dcdd238eb4648a8f52872ef991e
Ludivine Poussier <ludivinepoussier@hotmail.com>
2020-06-22 20:58:26 +0100

    datacountries 2.12 done

--- part-2/datacountries/src/components/List.js
+++ part-2/datacountries/src/components/List.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import Country from './Country'
+import Names from './Names'
 
 const List = ({ countries, searchTerm }) => {
-    return (
-        countries.filter(it => it.name.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0)
-            .map(it => (
-                <Country key={it.name} data={it} />
-            ))
 
-    )
+    if (!searchTerm) return <div></div>
+    
+    const filtered = countries.filter(it => it.name.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0)
+
+    if (filtered.length === 0) return <div>no macth found</div>
+    if (filtered.length === 1) return <Country data={filtered[0]} />
+    if (filtered.length < 11) return filtered
+        .map(it => (<Names key={it.name} data={it} />))
+    return <div>too many macthes</div>
 }
 
 export default List
