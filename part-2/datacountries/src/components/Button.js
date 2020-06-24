0b733bc357707ec29384d78337486cccac6beae9
Ludivine Poussier <ludivinepoussier@hotmail.com>
2020-06-24 13:43:34 +0100

    datacountries 2.13 Button component

---
+++ part-2/datacountries/src/components/Button.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+)
+
+export default Button
--- part-2/datacountries/src/components/CountryName.js
+++ part-2/datacountries/src/components/CountryName.js
@@ -1,10 +1,18 @@
 import React from "react";
+import Button from "./Button"
+import CountryDetails from "./CountryDetails";
+
+const handleClickShow = () => {
+    return (
+        <CountryDetails />
+    )
+}
 
 const CountryName = ({ data }) => {
 
     return (
         <ul>
-            <li>{data.name}</li>
+            <li>{data.name} <Button onClick={handleClickShow} text='Show'/></li>
         </ul>
 
     )
