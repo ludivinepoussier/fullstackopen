370b9108a0b043efd4506443e87959c84d163ba4
Ludivine Poussier <ludivinepoussier@hotmail.com>
2020-06-24 17:13:22 +0100

    datacountries 2.13 done

--- part-2/datacountries/src/components/CountryName.js
+++ part-2/datacountries/src/components/CountryName.js
@@ -1,20 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button"
-import CountryDetails from "./CountryDetails";
-
-const handleClickShow = () => {
-    return (
-        <CountryDetails />
-    )
-}
+import CountryDetails from "./CountryDetails"
 
 const CountryName = ({ data }) => {
+    const [clicked, setClicked] = useState(false)
+    const handleClickShow = () => setClicked(!clicked)
 
     return (
         <ul>
-            <li>{data.name} <Button onClick={handleClickShow} text='Show'/></li>
+            <li>
+                {data.name} <Button onClick={handleClickShow} text='Show' />
+                {clicked ? <CountryDetails data={data} /> : null}
+            </li>
         </ul>
-
     )
 }
 
