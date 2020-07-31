a27ed64ef18dcf149b1fcb8863ee9774ddcf7eb4
Ludivine Poussier <ludivinepoussier@hotmail.com>
2020-07-31 15:42:18 +0100

    3.11 phonebook full stack
    add build

---
+++ part-2/phonebook/build/precache-manifest.883a6c30a1c94d7874e2636ddd8f6929.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1686a42740b5e3a9201bc3e65d4ebaf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71e043e69fbf0a9eba22",
+    "url": "/static/css/main.e069b7b9.chunk.css"
+  },
+  {
+    "revision": "40cabd5745ae53a2eb5f",
+    "url": "/static/js/2.90dc4d32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90dc4d32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e043e69fbf0a9eba22",
+    "url": "/static/js/main.714002e1.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
