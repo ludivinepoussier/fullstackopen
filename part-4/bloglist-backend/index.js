89008b78cf51ee73c67076b4fa0f7e420b1bde51
Ludivine Poussier <ludivinepoussier@hotmail.com>
2020-08-11 16:19:44 +0100

    4.2 Blog list: refactor the application into separate modules

--- part-4/bloglist-backend/index.js
+++ part-4/bloglist-backend/index.js
@@ -1,85 +1,10 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const Blog = require('./models/blog')
+const app = require('./app') // the actual Express application
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const cors = require('cors')
+const server = http.createServer(app)
 
-const morganLog = morgan((tokens, req, res) => {
-    const customLog = [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms'
-    ].join(' ')
-
-    return req.method === 'POST' ? `${customLog} ${JSON.stringify(req.body)}` : customLog
-})
-
-app.use(cors())
-app.use(express.json())
-app.use(morganLog)
-
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.get('/api/blogs/:id', (request, response, next) => {
-    Blog.findById(request.params.id)
-        .then(blog => {
-            if (blog) {
-                response.json(blog)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
-app.delete('/api/blogs/:id', (request, response, next) => {
-    Blog.findByIdAndRemove(request.params.id)
-        .then(_result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))   
-})
-
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
-
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
-    next(error)
-}
-
-app.use(errorHandler)
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
--- part-4/bloglist-backend/models/blog.js
+++ part-4/bloglist-backend/models/blog.js
@@ -3,20 +3,6 @@ const mongoose = require('mongoose')
 // to avoid error message: DeprecationWarning
 mongoose.set('useFindAndModify', false)
 
-// connect to mongodb
-const mongourl = process.env.MONGODB_URI
-
-console.log('connecting to', mongourl)
-
-mongoose.connect(mongourl, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(_result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
-
-// model Blog
 const blogSchema = mongoose.Schema({
     title: String,
     author: String,
