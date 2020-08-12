e3cdf93ad3c9d8d949204f3d8b32bffd47d1288e
Ludivine <ludivinepoussier2012@gmail.com>
2020-08-12 16:44:07 +0100

    4.4: helper functions and unit test totalLikes

--- part-4/bloglist-backend/utils/list_helper.js
+++ part-4/bloglist-backend/utils/list_helper.js
@@ -1,8 +1,11 @@
-const dummy = (blogs) => {
+const dummy = blogs => {
   blogs = 1
   return blogs
 }
 
+const totalLikes = blogs => blogs.reduce(function (acc, obj) { return acc + obj.likes }, 0)
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
