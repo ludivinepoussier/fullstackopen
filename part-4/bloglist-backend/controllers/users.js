5ecc9d8f827a442e97d2006e2f9429a34b8e99f8
Ludivine <ludivinepoussier2012@gmail.com>
2020-08-20 16:15:46 +0100

    4.16*: password restrictions

--- part-4/bloglist-backend/controllers/users.js
+++ part-4/bloglist-backend/controllers/users.js
@@ -10,6 +10,9 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const body = request.body
 
+  if (body.password === undefined) { return response.status(400).json({ error: 'password missing' }) }
+  if (body.password.length < 3) { return response.status(400).json({ error: 'password must have at least 3 characters' }) }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
