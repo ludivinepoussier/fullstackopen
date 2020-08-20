5243bc7bb5a5d3abd34f479c0b8f4e5a75a2c4ad
Ludivine <ludivinepoussier2012@gmail.com>
2020-08-20 12:44:42 +0100

    4.15: users route

--- part-4/bloglist-backend/app.js
+++ part-4/bloglist-backend/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+const usersRouter = require('./controllers/users')
 const notesRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -21,7 +22,11 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', notesRouter)
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+
 module.exports = app
