36a777360b38f067d7b159ba4bd220ad7a5673e4
Ludivine <ludivinepoussier2012@gmail.com>
2020-08-21 14:54:43 +0100

    required title and url

--- part-4/bloglist-backend/models/blog.js
+++ part-4/bloglist-backend/models/blog.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose')
 mongoose.set('useFindAndModify', false)
 
 const blogSchema = mongoose.Schema({
-  title: String,
+  title: { type: String, required: true },
   author: String,
-  url: String,
+  url: { type: String, required: true },
   likes: { type: Number, default: 0 },
   user: {
     type: mongoose.Schema.Types.ObjectId,
