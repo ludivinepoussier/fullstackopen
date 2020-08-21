2c055dc99bafa3f6740dc0d51aa01887d8760bbc
Ludivine <ludivinepoussier2012@gmail.com>
2020-08-21 13:38:21 +0100

    4.17: User appears into blog route

--- part-4/bloglist-backend/controllers/blogs.js
+++ part-4/bloglist-backend/controllers/blogs.js
@@ -1,8 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find({}).find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs)
 })
 
@@ -18,6 +20,8 @@ blogsRouter.get('/:id', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
   if (body.title === undefined || body.url === undefined) {
     return response.status(400).end()
   }
@@ -26,10 +30,14 @@ blogsRouter.post('/', async (request, response) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.json(savedBlog)
 })
 
