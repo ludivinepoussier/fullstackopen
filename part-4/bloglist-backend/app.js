2e5333cc6b1e1c4e1852b44502ab5121454e102a
Ludivine <ludivinepoussier2012@gmail.com>
2020-08-21 13:58:31 +0100

    4.18: token authentication

--- part-4/bloglist-backend/app.js
+++ part-4/bloglist-backend/app.js
@@ -3,8 +3,11 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+
+const loginRouter = require('./controllers/login')
 const usersRouter = require('./controllers/users')
 const notesRouter = require('./controllers/blogs')
+
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -23,6 +26,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', notesRouter)
 
