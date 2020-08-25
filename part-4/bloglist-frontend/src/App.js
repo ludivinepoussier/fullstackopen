867449e283fc6cc7dfe8ed853f0e12e2bbc9de72
Ludivine <ludivinepoussier2012@gmail.com>
2020-08-25 16:00:07 +0100

    5.2: local storage and logout

--- part-4/bloglist-frontend/src/App.js
+++ part-4/bloglist-frontend/src/App.js
@@ -24,6 +24,15 @@ const App = () => {
     fetchData()
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const addBlog = async (event) => {
     event.preventDefault()
     const blogObject = {
@@ -56,6 +65,9 @@ const App = () => {
         username, password,
       })
 
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
@@ -68,6 +80,11 @@ const App = () => {
       setUsername('')
       setPassword('')
     }
+  }
+
+  const logout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
   }
 
   const loginForm = () => (
@@ -153,7 +170,12 @@ const App = () => {
       {user === null ?
         loginForm() :
         <div>
-          <p>{user.username} logged in</p>
+          <div>
+            <p>{user.username} logged in</p>
+            <button onClick={logout}>
+              logout
+            </button>
+          </div>
           {blogForm()}
           <h2>List of blogs</h2>
           {blogs.map(blog => <Blog key={blog.id} blog={blog} /> )}
