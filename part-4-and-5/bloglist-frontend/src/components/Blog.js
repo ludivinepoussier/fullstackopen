2b4e1e7a75d800246efa335d1161f532836e6f9c
Ludivine <ludivinepoussier2012@gmail.com>
2020-08-26 17:04:10 +0100

    5.7*: show and hide details of each blog

--- part-4-and-5/bloglist-frontend/src/components/Blog.js
+++ part-4-and-5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,37 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author}
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+  const notShowingDetails = { display: showDetails ? 'none' : '' }
+  const showingDetails = { display: showDetails ? '' : 'none' }
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const blogStyle = {
+    padding: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const paraStyle = {
+    marginTop: 0,
+    marginBottom: 0
+  }
+
+  return (
+  <div style={blogStyle}>
+    <div style={notShowingDetails}>
+      {blog.title} by {blog.author} <button onClick={toggleDetails}>show</button>
+    </div>
+    <div style={showingDetails}>
+      <p style={paraStyle}>{`${blog.title} by ${blog.author}`} <button onClick={toggleDetails}>hide</button> </p>
+      <p style={paraStyle}>{`url: ${blog.url}`}</p>
+        <p style={paraStyle}>{`likes: ${blog.likes}`} <button>like</button> </p>
+    </div>
   </div>
-)
+)}
 
 export default Blog
