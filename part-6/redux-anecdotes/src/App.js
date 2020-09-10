3206c63be12030b0bfdd09dd44358e6c82a4c28f
Ludivine <ludivinepoussier2012@gmail.com>
2020-09-10 15:19:55 +0100

    Add anecdote functionality

--- part-6/redux-anecdotes/src/App.js
+++ part-6/redux-anecdotes/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote } from './reducers/anecdoteReducer'
+import { voteAnecdote, createAnecdote } from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -9,6 +9,13 @@ const App = () => {
   const vote = id => {
     console.log('vote', id)
     dispatch(voteAnecdote(id))
+  }
+
+  const addAnecdote = (event) => {
+    event.preventDefault();
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(createAnecdote(content))
   }
 
   return (
@@ -26,9 +33,9 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      <form onSubmit={addAnecdote}>
+        <div><input name='anecdote'/></div>
+        <button type='submit'>create</button>
       </form>
     </div>
   )
--- part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -36,8 +36,22 @@ const reducer = (state = initialState, action) => {
         anecdote.id !== id ? anecdote : updatedAnecdote
       )
     }
+    case 'NEW_ANECDOTE': {
+      return [...state, action.data]
+    }
     default:
       return state
+  }
+}
+
+export const createAnecdote = content => {
+  return {
+    type: "NEW_ANECDOTE",
+    data: {
+      content,
+      id: getId(),
+      votes: 0
+    }
   }
 }
 
