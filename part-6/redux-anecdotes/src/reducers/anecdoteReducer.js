7c084ca4a3f14d56691b29bfcd10d38940545c89
Ludivine <ludivinepoussier2012@gmail.com>
2020-09-15 19:28:44 +0100

    6.13: fetch anecdotes from backend jsonserver

--- part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,32 +1,13 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-const initialState = anecdotesAtStart.map(asObject)
-
-const anecdoteReducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
+const anecdoteReducer = (state = [], action) => {
+  console.log("state now: ", state)
+  console.log("action", action)
   switch (action.type) {
-    case 'VOTE': {
+    case 'NEW_ANECDOTE':
+      return [...state, action.data]
+    case 'INIT_ANECDOTES':
+      return action.data
+    case 'VOTE':
       const id = action.data.id
-
       const votedAnecdote = state.find(it => it.id === id)
       const updatedAnecdote = {
         ...votedAnecdote,
@@ -35,10 +16,6 @@ const anecdoteReducer = (state = initialState, action) => {
       return state.map(anecdote =>
         anecdote.id !== id ? anecdote : updatedAnecdote
       )
-    }
-    case 'NEW_ANECDOTE': {
-      return [...state, action.data]
-    }
     default:
       return state
   }
@@ -49,16 +26,22 @@ export const createAnecdote = content => {
     type: "NEW_ANECDOTE",
     data: {
       content,
-      id: getId(),
       votes: 0
     }
+  }
+}
+
+export const initializeAnecdotes = anecdotes => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data: anecdotes
   }
 }
 
 export const voteAnecdote = id => {
   return {
     type: "VOTE",
-    data: { id }
+    data: id
   }
 }
 
