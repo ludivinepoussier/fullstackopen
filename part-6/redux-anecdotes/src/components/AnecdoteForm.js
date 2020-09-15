46c7f13effd0902af33a6b050bd01a96bd3d6d6c
Ludivine <ludivinepoussier2012@gmail.com>
2020-09-15 19:38:42 +0100

    6.14: new anecdotes stored in the backend

--- part-6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part-6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,16 +2,18 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from './../reducers/anecdoteReducer'
 import { notificationMessage, removeMessage } from './../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
 
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(newAnecdote))
 
     dispatch(notificationMessage({ message: `You added "${content}"` }))
 
--- part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part-6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -21,13 +21,10 @@ const anecdoteReducer = (state = [], action) => {
   }
 }
 
-export const createAnecdote = content => {
+export const createAnecdote = data => {
   return {
     type: "NEW_ANECDOTE",
-    data: {
-      content,
-      votes: 0
-    }
+    data
   }
 }
 
