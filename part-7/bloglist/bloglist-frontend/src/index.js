b7aefbeace9350166db908eb5e242eb724eb806b
Ludivine <ludivinepoussier2012@gmail.com>
2020-09-30 16:31:42 +0100

    comments

--- part-7/bloglist/bloglist-frontend/src/index.js
+++ part-7/bloglist/bloglist-frontend/src/index.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// import { createStore } from 'redux'
+// import { Provider } from 'react-redux'
 import App from './App'
+// import { blogformReducer } from './reducers/blogformReducer'
+// import { blogReducer } from './reducers/blogReducer'
+// import { notificationReducer } from './reducers/notificationReducer'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// const store = createStore(blogReducer)
+
+ReactDOM.render(
+  // <Provider store={store}>
+    <App />,
+  // </Provider>,
+  document.getElementById('root')
+)
