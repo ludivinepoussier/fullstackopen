5c2afa0ba2e2dfd0a27d26d9cac4e6c8d371a3f0
Ludivine <ludivinepoussier2012@gmail.com>
2020-10-21 17:13:09 +0100

    8.4: Books of one author only

--- part-8/library/library-backend/index.js
+++ part-8/library/library-backend/index.js
@@ -98,7 +98,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -107,7 +107,13 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if(!args.author) {
+        return books
+      }
+      return books.filter(book => book.author === args.author)
+    },
+
     allAuthors: () => {
       return authors.map(author => {
         const bookCount = books.filter(book => book.author === author.name).length
