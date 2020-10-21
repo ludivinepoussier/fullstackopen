a90b18ccee7c9830fe274deb1dfeb653d5618a63
Ludivine <ludivinepoussier2012@gmail.com>
2020-10-21 15:07:15 +0100

    8.1: queries bookCount and authorCount

--- part-8/library/library-backend/index.js
+++ part-8/library/library-backend/index.js
@@ -80,11 +80,15 @@ let books = [
 
 const typeDefs = gql`
   type Query {
+    bookCount: Int!
+    authorCount: Int!
   }
 `
 
 const resolvers = {
   Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length
   }
 }
 
