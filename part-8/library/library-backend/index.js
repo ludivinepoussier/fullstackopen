1b8412df4a8f016babccdc1e371a2b24a4b712ed
Ludivine <ludivinepoussier2012@gmail.com>
2020-10-21 15:18:37 +0100

    8.2: query to return the details of all books

--- part-8/library/library-backend/index.js
+++ part-8/library/library-backend/index.js
@@ -79,16 +79,27 @@ let books = [
 ]
 
 const typeDefs = gql`
+
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]
+    id: ID!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
