e7517084b0808efd6991d1ea849e16e1a5bc9324
Ludivine <ludivinepoussier2012@gmail.com>
2020-10-23 10:19:51 +0100

    8.5: Books by genre

--- part-8/library/library-backend/index.js
+++ part-8/library/library-backend/index.js
@@ -98,7 +98,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -108,10 +108,9 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if(!args.author) {
-        return books
-      }
-      return books.filter(book => book.author === args.author)
+      if (!args) return books
+      if (args.author) return books.filter(book => book.author === args.author)
+      if (args.genre) return books.filter(book => book.genres.includes(args.genre))
     },
 
     allAuthors: () => {
