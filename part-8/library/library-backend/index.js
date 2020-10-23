f56522183c9e98fc2e18a1b19a5e46bcb6cfe95d
Ludivine <ludivinepoussier2012@gmail.com>
2020-10-23 11:55:02 +0100

    8.7: Updating the birth year of an author

--- part-8/library/library-backend/index.js
+++ part-8/library/library-backend/index.js
@@ -110,6 +110,10 @@ const typeDefs = gql`
       author: String!
       genres: [String!]
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -145,12 +149,23 @@ const resolvers = {
       books = books.concat(book)
       return book
       }
-      
+
       const book = { ...args, id: uuid() }
       books = books.concat(book)
       const author = { name: args.author, id: uuid(), born: null }
       authors = authors.concat(author)
       return book
+    },
+    
+    editAuthor: (root, args) => {
+      const author = authors.find(author => author.name === args.name)
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(author => author.name === args.name ? updatedAuthor : author)
+      return updatedAuthor
     }
   }
 }
