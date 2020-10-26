f608856b0faeff6116a13fdae6990c3de328bcb3
Ludivine <ludivinepoussier2012@gmail.com>
2020-10-26 11:49:13 +0000

    8.9: fix allBooks resolver issue

--- part-8/library/library-backend/index.js
+++ part-8/library/library-backend/index.js
@@ -122,10 +122,10 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args) return books
       if (args.author && args.genre) return books.filter(book => book.author === args.author && book.genres.includes(args.genre))
       if (args.author) return books.filter(book => book.author === args.author)
       if (args.genre) return books.filter(book => book.genres.includes(args.genre))
+      return books
     },
 
     allAuthors: () => {
