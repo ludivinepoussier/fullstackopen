921b126b3ccacbe03e0331c4ac5481d5027fc5a9
Ludivine <ludivinepoussier2012@gmail.com>
2020-11-19 09:35:21 +0000

    9.7 WebExercises

--- part-9/exercises/exerciseCalculator.ts
+++ part-9/exercises/exerciseCalculator.ts
@@ -1,33 +1,33 @@
-interface CalculateValues {
-  value1: number[];
-  value2: number;
-}
+// interface CalculateValues {
+//   value1: number[];
+//   value2: number;
+// }
 
-const verifyArguments = (args: Array<string>): CalculateValues => {
-  if (args.length < 4) throw new Error('Not enough arguments');
+// const verifyArguments = (args: Array<string>): CalculateValues => {
+//   if (args.length < 4) throw new Error('Not enough arguments');
 
-  const inputValues = args.slice(2);
-  if (inputValues.some(it => isNaN(Number(it)) && it === undefined)) throw new Error('Provided values were not numbers!');
+//   const inputValues = args.slice(2);
+//   if (inputValues.some(it => isNaN(Number(it)) && it === undefined)) throw new Error('Provided values were not numbers!');
 
-  const inputNumbers = inputValues.map(it => Number(it));
+//   const inputNumbers = inputValues.map(it => Number(it));
 
-  return {
-    value1: inputNumbers,
-    value2: Number(inputNumbers.pop()),
-  };
-};
+//   return {
+//     value1: inputNumbers,
+//     value2: Number(inputNumbers.pop()),
+//   };
+// };
 
 interface calculatorResult {
   periodLength: number;
   trainingDays: number;
   success: boolean;
   rating: number;
-  ratingDescription: string;
+  ratingDescription: string | undefined;
   target: number;
   average: number;
 }
 
-const calculateExercises = (dailyHours: number[], targetNum: number): calculatorResult => {
+export const calculateExercises = (dailyHours: number[], targetNum: number): calculatorResult => {
   const periodLength = dailyHours.length;
   const trainingDays = dailyHours.filter(hours => hours > 0).length;
   const target = targetNum;
@@ -39,15 +39,15 @@ const calculateExercises = (dailyHours: number[], targetNum: number): calculator
 
   if (average > targetNum) {
     rating = 3;
-    ratingDescription = 'Well done, you did more than expected!';
+    ratingDescription = `Well done, you did more than expected!`;
   }
   else if (average < targetNum) {
     rating = 1;
-    ratingDescription = 'You could do better. Don\'t give up';
+    ratingDescription = `You could do better. Don't give up`;
   }
   else if (average === targetNum) {
     rating = 2;
-    ratingDescription = 'Well done, you achieved your goals!';
+    ratingDescription = `Well done, you achieved your goals!`;
   }
 
   return {
@@ -55,18 +55,18 @@ const calculateExercises = (dailyHours: number[], targetNum: number): calculator
     trainingDays,
     success,
     rating: Number(rating),
-    ratingDescription: JSON.stringify(ratingDescription),
+    ratingDescription: ratingDescription,
     target,
     average,
   };
 
 };
 
-try {
-  const { value1, value2 } = verifyArguments(process.argv);
-  console.log(calculateExercises(value1, value2));
-} catch (e) {
-  if (e instanceof Error) {
-    console.log('Error, something bad happened, message: ', e.message);
-  }
-}
+// try {
+//   const { value1, value2 } = verifyArguments(process.argv);
+//   console.log(calculateExercises(value1, value2));
+// } catch (e) {
+//   if (e instanceof Error) {
+//     console.log('Error, something bad happened, message: ', e.message);
+//   }
+// }
--- part-9/exercises/index.ts
+++ part-9/exercises/index.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from 'express';
 import calculateBmi from './calculateBmi';
+import { calculateExercises } from './exerciseCalculator';
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-app.get('/bmi', (req: Request, res: Response) => {
+app.get('/bmi', (req, res) => {
   const { weight, height } = req.query;
   if (!isNaN(Number(height)) && !isNaN(Number(weight))) {
     const bmi = calculateBmi(Number(height), Number(weight));
@@ -19,6 +21,27 @@ app.get('/bmi', (req: Request, res: Response) => {
       { error: "malformatted parameters" }
     );
   }
+});
+
+app.post('/exercises', (req: Request, res: Response) => {
+
+  type CalculateValues = {
+    daily_exercises: number[],
+    target: number,
+  };
+
+
+  const { daily_exercises, target } = req.body as CalculateValues;
+
+  if (!daily_exercises || !target) return res.json({ error: "parameters missing" });
+  if (typeof (target) !== "number" || daily_exercises.some(isNaN)) return res.json({ error: "malformatted parameters" });
+
+  const daily_exercisesTyped = daily_exercises.map(it => Number(it));
+  const targetTyped = Number(target);
+
+  const result = calculateExercises(daily_exercisesTyped, targetTyped);
+
+  return res.json(result);
 });
 
 const PORT = 3003;
