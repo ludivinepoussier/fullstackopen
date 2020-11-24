10b87658e1a1fbc02ec4b24341e20cb7ee4731bc
Ludivine <ludivinepoussier2012@gmail.com>
2020-11-24 17:01:57 +0000

    9.10: Patientor backend, step3

--- part-9/patientor/patientor-backend/src/index.ts
+++ part-9/patientor/patientor-backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import diagnoseRouter from './routes/diagnoses';
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ part-9/patientor/patientor-backend/src/services/diagnoseService.ts
@@ -0,0 +1,6 @@
+import diagnoses from '../data/diagnoses';
+import { Diagnose } from '../types';
+
+const getDiagnoses = (): Diagnose[] => diagnoses;
+
+export default { getDiagnoses }; 
